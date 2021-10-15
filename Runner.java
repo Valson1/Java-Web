3333b072ed126de1ed2c57b719be3eb3333841c8
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-10-15 20:57:05 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -3,10 +3,10 @@ import by.gsu.epamlab.Subject;
 public class Runner {
     public static void main(String[] args) {
 	final Material steel = new Material("Steel",7850.0);
-	Subject subject = new Subject("Wire",steel,0.03);
-	System.out.println(subject);
+	Subject wire = new Subject("Wire",steel,0.03);
+	System.out.println(wire);
 	final Material cooper = new Material("Cooper",8500.0);
-	subject.setMaterial(cooper);
-	System.out.println("The wire mass is: " + subject.getMass() + " kg");
+	wire.setMaterial(cooper);
+	System.out.println("The wire mass is: " + wire.getMass() + " kg");
     }
 }
