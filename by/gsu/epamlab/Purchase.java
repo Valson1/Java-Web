a8d488e7129dcb7dd0b52650d436a05d5ed4f77a
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-11-04 18:54:46 +0300

    Add files via upload

--- by/gsu/epamlab/Purchase.java
+++ by/gsu/epamlab/Purchase.java
@@ -1,37 +1,29 @@
 package by.gsu.epamlab;
 public class Purchase implements Comparable<Purchase> {
-    private final String name;
-    private int price;
+    private final static String NAME = "Milk";
+    private final static int PRICE = 120;
     private int numberOfPurchaseUnits;
     private int disountPercent;
     private WeekDay weekDay;
-    public Purchase(String name, int price, int numberOfPurchaseUnits,
-	    int disountPercent, WeekDay weekDay) {
-	super();
-	this.name = name;
-	this.price = price;
-	this.numberOfPurchaseUnits = numberOfPurchaseUnits;
-	this.disountPercent = disountPercent;
-	this.weekDay = weekDay;
+    public Purchase(int numberOfPurchaseUnits,int disountPercent, 
+    		WeekDay weekDay) {
+		super();
+		this.numberOfPurchaseUnits = numberOfPurchaseUnits;
+		this.disountPercent = disountPercent;
+		this.weekDay = weekDay;
     }
-    
     public Purchase() {
-	this.name = "";
-	
+    	
     }
 
     public String getName() {
-        return name;
+        return NAME;
     }
     
     public int getPrice() {
-        return price;
+        return PRICE;
     }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
+    
     public int getNumberOfPurchaseUnits() {
         return numberOfPurchaseUnits;
     }
@@ -56,13 +48,13 @@ public class Purchase implements Comparable<Purchase> {
         this.weekDay = weekDay;
     }
     public int getCost() {
-	return price * numberOfPurchaseUnits * (100 - disountPercent) / 100;
+    	return PRICE * numberOfPurchaseUnits * (100 - disountPercent) / 100;
     }
     
     @Override
     public String toString() {
-	return name + ";" + PurchaseAction.toRubles(price) + ";" + numberOfPurchaseUnits + ";" 
-		+ disountPercent + ";" + weekDay + ";" + PurchaseAction.roundtoRuble(getCost());
+	return NAME + ";" + toRubles(PRICE) + ";" + numberOfPurchaseUnits + ";" 
+		+ disountPercent + ";" + weekDay + ";" + roundtoRuble(getCost());
     }
     
     @Override
@@ -70,4 +62,28 @@ public class Purchase implements Comparable<Purchase> {
 	return this.numberOfPurchaseUnits > o.numberOfPurchaseUnits ? 
 		(this.numberOfPurchaseUnits == o.numberOfPurchaseUnits ? 0 : 1) : -1;
     }    
+    private String toRubles(int value) {
+    	return value / 100 + "." + value / 10 % 10 + value % 10;	
+	}
+    
+    private int roundtoRuble(int value) {
+		int valueBefore = value / 100;
+		int valueTenthPart = value / 10 % 10;
+		int valueHundredthPart = value % 10;
+		if(valueTenthPart != 0 || valueHundredthPart != 0) {
+		    if(valueHundredthPart >= 5) {
+			valueTenthPart += 1;
+		    }
+		    else {
+			valueTenthPart -= 1;
+		    }
+		    if (valueTenthPart >= 5) {
+			return (valueBefore + 1); 
+		    }
+		    else {
+			return (valueBefore - 1); 
+		    }
+		}
+		return valueBefore;
+	} 
 }
