9245dd1b6a5b94765c09bd2bc3f0e8313214ed79
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-11-04 18:51:41 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -2,50 +2,52 @@ import by.gsu.epamlab.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Runner {
-    public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		final int purchasesNumber = sc.nextInt();
-		Purchase []purchases = new Purchase[purchasesNumber];
+    public static void main(String[] args) {    	
 		try(Scanner file = new Scanner(new FileReader("src/in.txt"))) {
-			for (int i = 0;i < purchases.length; i++) {
-			    file.useLocale(Locale.ENGLISH);
-			    int numberOfPurchase = file.nextInt();
-			    String name = file.next();
-			    int price = file.nextInt();
+			final int PURCHASES_NUMBER = file.nextInt();
+			Purchase []purchases = new Purchase[PURCHASES_NUMBER];
+			for (int i = 0; i < purchases.length; i++){
+				file.useLocale(Locale.ENGLISH);
 			    int numberOfPurchaseUnits = file.nextInt();
 			    int discountPercent = file.nextInt();
 			    int day = file.nextInt();
-			    WeekDay weekDay = WeekDay.valueToDay(day);
-			    purchases[i] = new Purchase(name,price,numberOfPurchaseUnits,
+			    WeekDay weekDay = WeekDay.values()[day];
+			    purchases[i] = new Purchase(numberOfPurchaseUnits,
 				    discountPercent,weekDay);
 			    System.out.println(purchases[i]);
 		    }
+			Purchase maxCostOfPurchase = purchases[0];
+			int sumOfPurchasesCost = 0;
+			int sumOfPurchasesCostOnMonday = 0;
+			for (Purchase purchase : purchases) {
+				sumOfPurchasesCost += purchase.getCost();
+				if(purchase.getWeekDay() == WeekDay.MONDAY) {
+					sumOfPurchasesCostOnMonday += purchase.getCost();
+				}
+				if(maxCostOfPurchase.getCost() < purchase.getCost()) {
+					maxCostOfPurchase = purchase;
+				}
+			}
+			System.out.println("Mean cost " + sumOfPurchasesCost / 1000 + "." 
+					+ sumOfPurchasesCost % 1000);
+			System.out.println("Total cost on Monday " + sumOfPurchasesCostOnMonday / 100 + "."
+					+ sumOfPurchasesCostOnMonday / 10 % 10 
+					+ sumOfPurchasesCostOnMonday % 10);
+			System.out.println("The day with the maximum cost purchase "
+					+ maxCostOfPurchase.getWeekDay());
+			Arrays.sort(purchases);
+			for (Purchase purchase : purchases) {
+				System.out.println(purchase);
+			}
+			System.out.println(Arrays.binarySearch(purchases,purchases[4]));
 		} catch (FileNotFoundException e) {
 		    System.err.println("Input file is not found");
 		}
-		Purchase maxCostOfPurchase = purchases[0];
-		int sumOfPurchasesCost = 0;
-		int sumOfPurchasesCostOnMonday = 0;
-		for (Purchase purchase : purchases) {
-			sumOfPurchasesCost += purchase.getCost();
-			if(purchase.getWeekDay() == WeekDay.MONDAY) {
-			    sumOfPurchasesCostOnMonday += purchase.getCost();
-			}
-			if(maxCostOfPurchase.getCost() < purchase.getCost()) {
-			    maxCostOfPurchase = purchase;
-			}
-		}
-		Arrays.sort(purchases);
-		for (Purchase purchase : purchases) {
-		    System.out.println(purchase);
-		}
-		PurchaseAction.showCostInformation(sumOfPurchasesCost,
-			sumOfPurchasesCostOnMonday, maxCostOfPurchase);
-		PurchaseAction.findPurchase(purchases);
     }
 }
 
---
+++ WeekDay.java
@@ -0,0 +1,5 @@
+package by.gsu.epamlab;
+
+public enum WeekDay {
+    SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
+}
