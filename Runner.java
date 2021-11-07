bbf66153436c124245d1d906ccd228de16b20de4
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-11-08 01:07:19 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -8,35 +8,6 @@ import java.util.Scanner;
 import by.gsu.epamlab.*;
 
 public class Runner {
-    private static void showPurchases(Purchase[] purchases) {
-	System.out.println(Purchase.NAME + ";" + UtilityClass.toRubles(Purchase.PRICE));
-	for (Purchase purchase : purchases) {
-	    System.out.println(purchase);
-	}
-    }
-
-    private static void purchaseCostInformation(Purchase[] purchases) {
-	WeekDay maxCostDay = null;
-	int sumOfPurchasesCostOnMonday = 0;
-	double meanCost = 0;
-	int maxCost = purchases[0].getCost();
-	for (Purchase purchase : purchases) {
-	    int cost = purchase.getCost();
-	    meanCost += cost;
-	    if (purchase.getWeekDay() == WeekDay.MONDAY) {
-		sumOfPurchasesCostOnMonday += cost;
-	    }
-	    if (maxCost < cost) {
-		maxCostDay = purchase.getWeekDay();
-	    }
-	}
-	if (purchases.length > 0) {
-	    meanCost /= purchases.length;
-	}
-	System.out.printf(Locale.ENGLISH, "The mean cost of all purchases: %.3f\n", meanCost / 100);
-	System.out.println("Total cost on Monday " + UtilityClass.toRubles(sumOfPurchasesCostOnMonday));
-	System.out.println("The day with the maximum cost purchase " + maxCostDay);
-    }
 
     public static void main(String[] args) {
 
@@ -68,13 +39,42 @@ public class Runner {
 	    // 7. Find Purchase with number of units by binarySearch();
 	    int valueOfSearch = Arrays.binarySearch(purchases, new Purchase(5, 0, 0));
 	    if (valueOfSearch >= 0) {
-		System.out.println(
-			"Required purchase is " + purchases[valueOfSearch]);
+		System.out.println("Required purchase is " + purchases[valueOfSearch]);
 	    } else {
 		System.out.println("Requried purchase is not found");
 	    }
 	} catch (FileNotFoundException e) {
 	    System.err.println("Input file is not found");
 	}
+    }
+
+    private static void showPurchases(Purchase[] purchases) {
+	System.out.println(Purchase.NAME + ";" + UtilityClass.toRubles(Purchase.PRICE));
+	for (Purchase purchase : purchases) {
+	    System.out.println(purchase);
+	}
+    }
+
+    private static void purchaseCostInformation(Purchase[] purchases) {
+	WeekDay maxCostDay = null;
+	int sumOfPurchasesCostOnMonday = 0;
+	double meanCost = 0;
+	int maxCost = 0;
+	for (Purchase purchase : purchases) {
+	    int cost = purchase.getCost();
+	    meanCost += cost;
+	    if (purchase.getWeekDay() == WeekDay.MONDAY) {
+		sumOfPurchasesCostOnMonday += cost;
+	    }
+	    if (maxCost < cost) {
+		maxCostDay = purchase.getWeekDay();
+	    }
+	}
+	if (purchases.length > 0) {
+	    meanCost /= purchases.length;
+	}
+	System.out.printf(Locale.ENGLISH, "The mean cost of all purchases: %.3f\n", meanCost / 100);
+	System.out.println("Total cost on Monday " + UtilityClass.toRubles(sumOfPurchasesCostOnMonday));
+	System.out.println("The day with the maximum cost purchase " + maxCostDay);
     }
 }
