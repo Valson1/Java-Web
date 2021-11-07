7e0a15ffae3b4fa43faf7114078b9a51183c635b
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-11-08 00:58:31 +0300

    Add files via upload

--- by/gsu/epamlab/Purchase.java
+++ by/gsu/epamlab/Purchase.java
@@ -1,59 +1,65 @@
 package by.gsu.epamlab;
+
 import static java.lang.Math.round;
+
 public class Purchase implements Comparable<Purchase> {
     public final static String NAME = "Milk";
     public final static int PRICE = 120;
     private int numberOfPurchaseUnits;
-    private double disountPercent;
+    private double disñountPercent;
     private WeekDay weekDay;
-    public Purchase(int numberOfPurchaseUnits,double disountPercent, 
-    		WeekDay weekDay) {
-		super();
-		this.numberOfPurchaseUnits = numberOfPurchaseUnits;
-		this.disountPercent = disountPercent;
-		this.weekDay = weekDay;
+
+    public Purchase(int numberOfPurchaseUnits, double disountPercent, WeekDay weekDay) {
+	super();
+	this.numberOfPurchaseUnits = numberOfPurchaseUnits;
+	this.disñountPercent = disountPercent;
+	this.weekDay = weekDay;
     }
-    public Purchase(int numberOfPurchaseUnits,double disountPercent, 
-    		int weekDay) {
-    	this(numberOfPurchaseUnits,disountPercent,WeekDay.values()[weekDay]);
+
+    public Purchase(int numberOfPurchaseUnits, double disountPercent, int weekDay) {
+	this(numberOfPurchaseUnits, disountPercent, WeekDay.values()[weekDay]);
     }
+
     public Purchase() {
-    	
+
     }
+
     public int getNumberOfPurchaseUnits() {
-        return numberOfPurchaseUnits;
+	return numberOfPurchaseUnits;
     }
 
     public void setNumberOfPurchaseUnits(int numberOfPurchaseUnits) {
-        this.numberOfPurchaseUnits = numberOfPurchaseUnits;
+	this.numberOfPurchaseUnits = numberOfPurchaseUnits;
     }
 
     public double getDisountPercent() {
-        return disountPercent;
+	return disñountPercent;
     }
 
     public void setDisountPercent(int disountPercent) {
-        this.disountPercent = disountPercent;
+	this.disñountPercent = disountPercent;
     }
 
     public WeekDay getWeekDay() {
-        return weekDay;
+	return weekDay;
     }
 
     public void setWeekDay(WeekDay weekDay) {
-        this.weekDay = weekDay;
+	this.weekDay = weekDay;
     }
+
     public int getCost() {
-    	return (int)round((PRICE * numberOfPurchaseUnits * (100 - disountPercent) / 100) / 100) * 100;
+	return (int) round((PRICE * numberOfPurchaseUnits * (100 - disñountPercent) / 100) / 100) * 100;
     }
+
     @Override
     public String toString() {
-    	return numberOfPurchaseUnits + ";" 
-			+ disountPercent + ";" + weekDay + ";" + UtilityClass.toRubles(getCost());
+	return numberOfPurchaseUnits + ";" 
+		+ disñountPercent + ";" + weekDay + ";" + UtilityClass.toRubles(getCost());
     }
-    
+
     @Override
     public int compareTo(Purchase purchase) {
-    	return this.numberOfPurchaseUnits - purchase.numberOfPurchaseUnits;
-    }    
+	return this.numberOfPurchaseUnits - purchase.numberOfPurchaseUnits;
+    }
 }
