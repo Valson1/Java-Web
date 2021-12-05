d11d780c690de199b96f4fc73fe9b3774d203deb
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2021-12-05 17:36:55 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -4,7 +4,7 @@ import by.gsu.epamlab.*;
 
 public class Runner {
     public static void main(String[] args) {
-	Product product = new Product("Milk", new Byn(100));
+	Product product = new Product("Milk", new Byn(123));
 	AbstractPurchase[] purchases = { new NumberDiscountPurchase(product, 12, new Byn(30)),
 		new PercentageDiscountPurchase(product, 3, 10), new NumberDiscountPurchase(product, 5, new Byn(0)),
 		new TransportExpencesPurchase(product, 7, new Byn(50)), new PercentageDiscountPurchase(product, 10, 20),
@@ -13,16 +13,16 @@ public class Runner {
 	Arrays.sort(purchases);
 	showPurchasesInformation(purchases);
 	System.out.println("Minimum cost of purchase:" + purchases[purchases.length - 1].getCost());
-	search(purchases, new NumberDiscountPurchase(product, 5, new Byn(0)));
-    }
-
-    private static void search(AbstractPurchase[] purchases, AbstractPurchase purchase) {
-	int index = Arrays.binarySearch(purchases, purchase);
+	int index = search(purchases, new NumberDiscountPurchase(product, 4, new Byn(0)));
 	if (index >= 0) {
 	    System.out.println("Purchase is found :" + purchases[index]);
 	} else {
 	    System.out.println("Purchase is not found");
 	}
+    }
+
+    private static int search(AbstractPurchase[] purchases, AbstractPurchase purchase) {
+	return Arrays.binarySearch(purchases, purchase);
     }
 
     private static void showPurchasesInformation(AbstractPurchase[] purchases) {
