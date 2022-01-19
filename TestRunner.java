94be439c2ba8a0b65f87b6e3593f2916e3afaf65
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-01-19 21:46:49 +0300

    Add files via upload

--- TestRunner.java
+++ TestRunner.java
@@ -10,64 +10,85 @@ public class TestRunner {
 
     private static int getResult(String csvName, StringBuilder strResult) throws FileNotFoundException {
 	try (Scanner sc = new Scanner(new FileReader(csvName))) {
+	    final String CSV_SEPARATOR = ";";
+	    final String MINUS = " - ";
+	    final String PLUS = " + ";
+	    final String RESULT_HEAD = "result(";
+	    final String RESULT_TAIL = ") = ";
 	    double result = 0;
 	    int errorLines = 0;
+	    int numLine = 0;
 	    while (sc.hasNext()) {
-		String line = sc.nextLine();
-		String[] elements = line.split(";");
-		if (isInt(elements[0])) {
-		    int firstElementValue = Integer.parseInt(elements[0]);
-		    if (checkFirstElementValue(firstElementValue, elements.length)
-			    && isDouble(elements[firstElementValue])) {
-			result += Double.parseDouble(elements[firstElementValue]);
+		String[] elements = sc.nextLine().split(CSV_SEPARATOR);
+		try {
+		    double elementValueOfFirstIElementIndexValue = Double
+			    .parseDouble(elements[Integer.parseInt(elements[0])]);
+		    result += elementValueOfFirstIElementIndexValue;
+		    numLine++;
+		    if (elementValueOfFirstIElementIndexValue < 0 && numLine > 1) {
+			strResult.append(MINUS).append(elementValueOfFirstIElementIndexValue * -1);
 		    } else {
-			errorLines++;
+			strResult.append(PLUS).append(elementValueOfFirstIElementIndexValue);
 		    }
-		} else {
+		} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
 		    errorLines++;
 		}
 	    }
-	    strResult.append(result);
+	    strResult.delete(0, 3);
+	    strResult.insert(0, RESULT_HEAD).append(RESULT_TAIL).append(result);
 	    return errorLines;
 	}
     }
 
-    private static boolean isInt(String s) throws NumberFormatException {
-	boolean isNumeric = false;
-	try {
-	    Integer.parseInt(s);
-	    isNumeric = true;
-	} catch (NumberFormatException e) {
-	}
-	return isNumeric;
-    }
-
-    private static boolean isDouble(String s) throws NumberFormatException {
-	boolean isNumeric = false;
-	try {
-	    Double.parseDouble(s);
-	    isNumeric = true;
-	} catch (NumberFormatException e) {
-	}
-	return isNumeric;
-    }
-
-    private static boolean checkFirstElementValue(int firstElementValue, int arrayLength) {
-	return firstElementValue < arrayLength && firstElementValue > 0;
-    }
-
     @Test
     public void testMainScenario() throws FileNotFoundException {
-	StringBuilder result = new StringBuilder();
-	int errorLines = getResult("src/src.txt", result);
-	Assert.assertEquals(3, errorLines);
-	String expected = "0.0";
-	Assert.assertEquals(expected, result.toString());
+	StringBuilder resultFile1 = new StringBuilder();
+	StringBuilder resultFile2 = new StringBuilder();
+	StringBuilder resultFile3 = new StringBuilder();
+	StringBuilder resultFile4 = new StringBuilder();
+	StringBuilder resultFile5 = new StringBuilder();
+
+	final String FILE1_NAME = "src/src1.txt";
+	final String FILE2_NAME = "src/src2.txt";
+	final String FILE3_NAME = "src/src3.txt";
+	final String FILE4_NAME = "src/src4.txt";
+	final String FILE5_NAME = "src/src5.txt";
+
+	int errorLinesFile1 = getResult(FILE1_NAME, resultFile1);
+	int errorLinesFile2 = getResult(FILE2_NAME, resultFile2);
+	int errorLinesFile3 = getResult(FILE3_NAME, resultFile3);
+	int errorLinesFile4 = getResult(FILE4_NAME, resultFile4);
+	int errorLinesFile5 = getResult(FILE5_NAME, resultFile5);
+
+	final int EXPECTED_ERROR_LINES_FILE1 = 3;
+	final int EXPECTED_ERROR_LINES_FILE2 = 0;
+	final int EXPECTED_ERROR_LINES_FILE3 = 0;
+	final int EXPECTED_ERROR_LINES_FILE4 = 0;
+	final int EXPECTED_ERROR_LINES_FILE5 = 1;
+
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE1, errorLinesFile1);
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE2, errorLinesFile2);
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE3, errorLinesFile3);
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE4, errorLinesFile4);
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE5, errorLinesFile5);
+	Assert.assertEquals(EXPECTED_ERROR_LINES_FILE5, errorLinesFile5);
+
+	final String EXPECTED_RESULT_FILE1 = "result(5.2 - 3.14 + 0.0) = 2.06";
+	final String EXPECTED_RESULT_FILE2 = "result(-3.1 - 1.0) = -4.1";
+	final String EXPECTED_RESULT_FILE3 = "result(0.75) = 0.75";
+	final String EXPECTED_RESULT_FILE4 = "result(0.0) = 0.0";
+	final String EXPECTED_RESULT_FILE5 = "result() = 0.0";
+
+	Assert.assertEquals(EXPECTED_RESULT_FILE1, resultFile1.toString());
+	Assert.assertEquals(EXPECTED_RESULT_FILE2, resultFile2.toString());
+	Assert.assertEquals(EXPECTED_RESULT_FILE3, resultFile3.toString());
+	Assert.assertEquals(EXPECTED_RESULT_FILE4, resultFile4.toString());
+	Assert.assertEquals(EXPECTED_RESULT_FILE5, resultFile5.toString());
+
     }
 
     @Test(expected = FileNotFoundException.class)
     public void testWrongCsvName() throws FileNotFoundException {
-	StringBuilder result = new StringBuilder();
-	int errorLines = getResult("src/srcwefw.txt", result);
+	getResult("", new StringBuilder(""));
     }
 }
