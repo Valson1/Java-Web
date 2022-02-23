902c4005a5cce57f75b49f787ce8aebbc208981f
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-02-23 12:59:02 +0300

    Add files via upload

--- TestRunner.java
+++ TestRunner.java
@@ -1,79 +1,103 @@
-import java.io.FileNotFoundException;
 import java.util.Enumeration;
-import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRunner {
     private static final String FILE_NAME1 = "resources.in1";
     private static final String FILE_NAME2 = "resources.in2";
+    private static final String FILE_NAME3 = "resources.in3";
 
-    private static final String RESULT = "sum = ";
-    private static final String INDEX = "index";
-    private static final String WITHOUT_INDEX = "[^(index)].*";
-    private static final String NATURAL_NUMBER = "[1-9]+[0-9]*";
+    private static final double EXPECTED_RESULT1 = 8.24;
+    private static final double EXPECTED_RESULT2 = 30.242;
+    private static final double EXPECTED_RESULT3 = 1.9;
 
-    private static final String EXPECTED_STRING_RESULT1 = String.format("%s8.24", RESULT);
-    private static final String EXPECTED_STRING_RESULT2 = String.format("%s30.242", RESULT);
     private static final int EXPECTED_ERROR_LINES1 = 3;
     private static final int EXPECTED_ERROR_LINES2 = 9;
+    private static final int EXPECTED_ERROR_LINES3 = 0;
 
-    private static int getResult(StringBuilder strResult, String fileName) throws MissingResourceException {
+    static class ErrorTest {
+	private int errorLines;
+	private double result;
+
+	public ErrorTest(int errorLines, double result) {
+	    this.errorLines = errorLines;
+	    this.result = result;
+	}
+
+	public ErrorTest() {
+	}
+
+	public int getErrorLines() {
+	    return errorLines;
+	}
+
+	public double getResult() {
+	    return result;
+	}
+
+    }
+
+    private static ErrorTest getResult(String fileName) {
+	final String VALUE = "value";
+	final String INDEX = "index(.*)";
+	final String NATURAL_NUMBER = "[1-9]\\d*";
 	ResourceBundle rb = ResourceBundle.getBundle(fileName);
 	int errorLines = 0;
 	double result = 0;
 	Enumeration<String> keys = rb.getKeys();
 	String key;
-	String keyNumPlusValue = "";
 	while (keys.hasMoreElements()) {
 	    key = keys.nextElement();
-	    Matcher index = Pattern.compile(INDEX).matcher(key);
-	    Matcher keyValue = Pattern.compile(NATURAL_NUMBER).matcher(rb.getString(key));
-	    Matcher withoutIndex = Pattern.compile(WITHOUT_INDEX).matcher(key);
-	    System.out.println(key + "=" + rb.getString(key));
-	    try {
-		if (index.lookingAt()) {
-		    withoutIndex.find();
-		    if (Pattern.matches(NATURAL_NUMBER, withoutIndex.group())) {
-			keyValue.lookingAt();
-			keyNumPlusValue = withoutIndex.group() + keyValue.group();
-			System.out.println(keyNumPlusValue);
-		    } else {
+	    Matcher indexMatcher = Pattern.compile(INDEX).matcher(key);
+	    if (indexMatcher.matches()) {
+		String indexNum = indexMatcher.group(1);
+		String indexValue = rb.getString(key);
+		Matcher indexNumMatcher = Pattern.compile(NATURAL_NUMBER).matcher(indexNum);
+		Matcher indexValueMatcher = Pattern.compile(NATURAL_NUMBER).matcher(indexValue);
+		if (indexValueMatcher.matches() && indexNumMatcher.matches()) {
+		    try {
+			result += Double.parseDouble(rb.getString(VALUE + indexNum + indexValue).trim());
+		    } catch (MissingResourceException | NumberFormatException e) {
 			errorLines++;
-			continue;
 		    }
+		} else {
+		    errorLines++;
 		}
-		
-	
-	    } catch (NumberFormatException | IllegalStateException e) {
-		errorLines++;
 	    }
 	}
-	strResult.append(RESULT).append(result);
-	return errorLines;
-
+	return new ErrorTest(errorLines, result);
     }
 
     @Test
     public void testMainScenario() {
-	StringBuilder result1 = new StringBuilder();
-	StringBuilder result2 = new StringBuilder();
-	
-	int errorLines1 = getResult(result1, FILE_NAME1);
-	int errorLines2 = getResult(result2, FILE_NAME2);
-	
-	Assert.assertEquals(errorLines1, EXPECTED_ERROR_LINES1);
-	Assert.assertEquals(errorLines2, EXPECTED_ERROR_LINES2);
-	
-	Assert.assertEquals(result1, EXPECTED_STRING_RESULT1);
-	Assert.assertEquals(result2, EXPECTED_STRING_RESULT2);
-	
-    }
-}
+	class TestCase {
+	    private ErrorTest errorTest;
+	    private String fileName;
+	    private static final double DELTA = 0.000000000000001;
 
-    
+	    public TestCase(ErrorTest errorTest, String fileName) {
+		this.errorTest = errorTest;
+		this.fileName = fileName;
+	    }
+	}
+	TestCase[] testCases = { new TestCase(new ErrorTest(EXPECTED_ERROR_LINES1, EXPECTED_RESULT1), FILE_NAME1),
+		new TestCase(new ErrorTest(EXPECTED_ERROR_LINES2, EXPECTED_RESULT2), FILE_NAME2),
+		new TestCase(new ErrorTest(EXPECTED_ERROR_LINES3, EXPECTED_RESULT3), FILE_NAME3), };
+
+	for (TestCase testCase : testCases) {
+	    ErrorTest test = getResult(testCase.fileName);
+	    Assert.assertEquals(testCase.errorTest.errorLines, test.errorLines);
+	    Assert.assertEquals(testCase.errorTest.result, test.result, TestCase.DELTA);
+	}
+    }
+
+    @Test(expected = MissingResourceException.class)
+    public void testFileName() {
+	getResult("erdf");
+    }
+
+}
