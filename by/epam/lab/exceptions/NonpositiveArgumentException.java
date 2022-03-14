c47dd40881a162d9039201ffa31135efe0c82033
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-03-14 21:55:14 +0300

    Delete NonpositiveArgumentException.java

--- by/epam/lab/exceptions/NonpositiveArgumentException.java
+++
@@ -1,9 +0,0 @@
-package by.epam.lab.exceptions;
-
-public class NonpositiveArgumentException extends IllegalArgumentException {
-
-    public NonpositiveArgumentException(String message) {
-	super(message);
-    }
-
-}
