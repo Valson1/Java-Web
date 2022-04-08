2824a2f3597fba2dd02aef6727be204a0f8a34d5
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-04-08 10:51:40 +0300

    Add files via upload

---
+++ by/epam/lab/utils/ConstantsUtility.java
@@ -0,0 +1,19 @@
+package by.epam.lab.utils;
+
+public class ConstantsUtility {
+    public static final String ELEMENT_SEPARATOR = "=";
+
+    public static final String FILE_NOT_FOUND = "File is not found";
+    public static final String FILE_NAME1 = "src/in1.txt";
+    public static final String FILE_NAME2 = "src/in2.txt";
+    public static final String FILE_NAME3 = "src/in3.txt";
+    public static final String FILE_NAME4 = "src/in4.txt";
+
+    public static final int X1_INDEX = 1;
+    public static final int Y1_INDEX = 2;
+    public static final int X2_INDEX = 3;
+    public static final int Y2_INDEX = 4;
+
+    public static final String REGEX = "[\\s(;)]+";
+
+}
