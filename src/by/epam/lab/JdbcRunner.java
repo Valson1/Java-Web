5bec6bc419be8d6315dc3eb60eeea69f5e9d8057
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-04-12 10:57:15 +0300

    Add files via upload

--- src/by/epam/lab/JdbcRunner.java
+++ src/by/epam/lab/JdbcRunner.java
@@ -2,6 +2,7 @@ package by.epam.lab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,58 +12,45 @@ import java.util.List;
 import by.epam.lab.beans.*;
 
 public class JdbcRunner {
-    private final static String CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private final static String SEPARATOR = ";";
+
     private final static String DB_URL = "jdbc:mysql://localhost:3306/segments";
     private final static String DB_PASSWORD = "Qwerty147258369";
     private final static String DB_USER = "root";
-    private final static String SQL_QUERY_SELECT_COORDINATES = "SELECT ABS(CEIL(x1-x2)) AS len, COUNT(*) AS num FROM Coordinates GROUP BY len ORDER BY len";
-    private final static String SQL_QUERY_DELETE_FREQUENCIES = "DELETE FROM Frequencies";
-    private final static String SQL_QUERY_SELECT_FREQUENCIES = "SELECT len,num FROM Frequencies";
-    private final static String SQL_QUERY_LEN_MORE_NUM_FREQUENCIES = "SELECT (len > num) FROM Frequencies;";
+
+    private final static int LEN_COLUMN = 1;
+    private final static int NUM_COLUMN = 2;
+
+    private final static String SELECT_COORDINATES = "SELECT ROUND(ABS(x2-x1)) AS len, COUNT(*) AS num FROM Coordinates GROUP BY 1 ORDER BY 1";
+    private final static String DELETE_FREQUENCIES = "DELETE FROM Frequencies";
+    private final static String INSERT_FREQUENCIES = "INSERT INTO Frequencies(len, num) VALUES (?, ?)";
+    private final static String LEN_MORE_NUM_FREQUENCIES = "SELECT len,num FROM Frequencies WHERE len > num;";
 
     public static void main(String[] args) {
-	try {
-	    Class.forName(CLASS_NAME);
-	    Connection cn = null;
-	    Statement st = null;
-	    ResultSet rs = null;
-	    try {
-		cn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-		st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-		rs = st.executeQuery(SQL_QUERY_SELECT_COORDINATES);
-		List<SegmentNumber> segments = new ArrayList<>();
+	try (Connection cn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		Statement st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+	    List<SegmentNumber> segments = new ArrayList<>();
+	    st.executeUpdate(DELETE_FREQUENCIES);
+	    try (ResultSet rs = st.executeQuery(SELECT_COORDINATES)) {
 		while (rs.next()) {
-		    segments.add(new SegmentNumber(rs.getInt(1), rs.getInt(2)));
-		}
-		st.executeUpdate(SQL_QUERY_DELETE_FREQUENCIES);
-		rs = st.executeQuery(SQL_QUERY_SELECT_FREQUENCIES);
-		for (SegmentNumber segmentNumber : segments) {
-		    rs.moveToInsertRow();
-		    rs.updateObject(1, segmentNumber.getLen());
-		    rs.updateObject(2, segmentNumber.getNum());
-		    rs.insertRow();
-		    rs.moveToCurrentRow();
-		}
-		rs = st.executeQuery(SQL_QUERY_LEN_MORE_NUM_FREQUENCIES);
-		System.out.println(segments);
-		for (SegmentNumber segmentNumber : segments) {
-		    rs.next();
-		    if (rs.getInt(1) == 1) {
-			System.out.println(segmentNumber);
-		    }
-		}
-	    } finally {
-		if (rs != null && !rs.isClosed()) {
-		    rs.close();
-		}
-		if (st != null) {
-		    st.close();
-		}
-		if (cn != null) {
-		    cn.close();
+		    segments.add(new SegmentNumber(rs.getInt(LEN_COLUMN), rs.getInt(NUM_COLUMN)));
 		}
 	    }
-	} catch (SQLException | ClassNotFoundException e) {
+	    try (PreparedStatement ps = cn.prepareStatement(INSERT_FREQUENCIES)) {
+		for (SegmentNumber segmentNumber : segments) {
+		    ps.setInt(LEN_COLUMN, segmentNumber.getLen());
+		    ps.setInt(NUM_COLUMN, segmentNumber.getNum());
+		    ps.addBatch();
+		    System.out.println(segmentNumber);
+		}
+		ps.executeBatch();
+	    }
+	    try (ResultSet rs = st.executeQuery(LEN_MORE_NUM_FREQUENCIES)) {
+		while (rs.next()) {
+		    System.out.println(rs.getInt(LEN_COLUMN) + SEPARATOR + rs.getInt(NUM_COLUMN));
+		}
+	    }
+	} catch (SQLException e) {
 	    e.printStackTrace();
 	}
     }
