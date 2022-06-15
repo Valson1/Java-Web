c76051640e846389612098bad6f84f93cbd595b4
Valiantsin Zharnasek <85641672+Valson1@users.noreply.github.com>
2022-06-15 21:09:58 +0300

    Delete Runner.java

--- src/Runner.java
+++
@@ -1,9 +0,0 @@
-import by.epam.lab.services.MarkKind;
-
-public class Runner {
-    private final static String FILE_NAME = "src/data/results1.csv";
-
-    public static void main(String[] args) {
-	MainLogic.logic(FILE_NAME, MarkKind.MARK);
-    }
-}
